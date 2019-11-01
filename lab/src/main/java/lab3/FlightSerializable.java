6629140307792c86b5e6c1a60670eeda99675bbc
alevko55555 <alevko55555@gmail.com>
2019-11-01 22:04:34 +0300

    Scripted auto-commit on change (2019-11-01 22:04:34) by gitwatch.sh

--- lab/src/main/java/lab3/FlightSerializable.java
+++ lab/src/main/java/lab3/FlightSerializable.java
@@ -3,13 +3,31 @@ package lab3;
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
+    private static final long ZERO = 0;
     private long maximumLatency;
     private long numberOfFlights;
     private long numberOfDelayedFlights;
     private long canceledFlight;
-    public FlightSerializable(long minValue, int i, int i1, int i2) {
+    public FlightSerializable(long maximumLatency, long numberOfFlights, long numberOfDelayedFlights, long canceledFlight) {
+        this.maximumLatency = maximumLatency;
+        this.numberOfFlights = numberOfFlights;
+        this.numberOfDelayedFlights = numberOfDelayedFlights;
+        this.canceledFlight = canceledFlight;
     }
     public FlightSerializable(){
-        this(Long.MIN_VALUE, 0, 0, 0);
+        this(Long.MIN_VALUE, ZERO, ZERO, ZERO);
+    }
+    
+    public long getMaximumLatency() {
+        return maximumLatency;
+    }
+    public long getNumberOfFlights() {
+        return  numberOfFlights;
+    }
+    public long getNumberOfDelayedFlights() {
+        return numberOfDelayedFlights;
+    }
+    public long getCanceledFlight() {
+        return canceledFlight;
     }
 }

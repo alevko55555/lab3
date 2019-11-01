108f894a4e126ca7ff9327d8ff885957e4570b4f
alevko55555 <alevko55555@gmail.com>
2019-11-01 22:39:14 +0300

    Scripted auto-commit on change (2019-11-01 22:39:14) by gitwatch.sh

--- lab/src/main/java/lab3/FlightParser.java
+++ lab/src/main/java/lab3/FlightParser.java
@@ -1,8 +1,10 @@
 package lab3;
 
 public class FlightParser extends BaseParser{
-    private static final int DELAY_COL = 14;
-    private static final int DELAY_NEW_COL = 18;
+    private static final int ID_AIRPORT_TO = 14;
+    private static final int TIME_DELAY = 18;
+    private static final int ID_AIRPORT_FROM = 11;
+    private static final int FLAG_CANCELED = 19;
     private static final  String ZERO_DOUBLE = "0.0";
 
     public FlightParser(String str) {

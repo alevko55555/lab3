efe9fa8c66ee34ac9c9b12eb80525c68521ef765
alevko55555 <alevko55555@gmail.com>
2019-11-01 20:07:11 +0300

    Scripted auto-commit on change (2019-11-01 20:07:11) by gitwatch.sh

--- lab/src/main/java/lab3/BaseParser.java
+++ lab/src/main/java/lab3/BaseParser.java
@@ -1,4 +1,18 @@
-package lab3;
+package lab2;
 
-public class BaseParser {
+import java.util.ArrayList;
+
+public class ParserUtils {
+    private ArrayList<String> part;
+
+    ParserUtils(String str) {
+        part = new ArrayList<>();
+        String[] part1 = str.split(",");
+        for(int i = 0; i < part1.length; i++) {
+            part.add(part1[i].replaceAll("\"", ""));
+        }
+    }
+    String getString(int index) {
+        return part.get(index);
+    }
 }

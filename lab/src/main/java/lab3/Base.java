8da80877611b7e1c19cc35783e1b73e4ade363ed
alevko55555 <alevko55555@gmail.com>
2019-11-01 19:26:28 +0300

    Scripted auto-commit on change (2019-11-01 19:26:28) by gitwatch.sh

--- lab/src/main/java/lab3/Base.java
+++ lab/src/main/java/lab3/Base.java
@@ -3,7 +3,9 @@ package lab3;
 public class Base {
     public static void main(String[] args) {
         if(args.length != 3) {
-            
+            System.err.println("Usage: lab3.Base <input path> <output path>");
+            System.exit(-1);
         }
+        
     }
 }

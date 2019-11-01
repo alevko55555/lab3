07b5376b8c6046b0f1346cc04696a597976ca36f
alevko55555 <alevko55555@gmail.com>
2019-11-01 19:29:10 +0300

    Scripted auto-commit on change (2019-11-01 19:29:10) by gitwatch.sh

--- lab/src/main/java/lab3/Base.java
+++ lab/src/main/java/lab3/Base.java
@@ -1,11 +1,15 @@
 package lab3;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class Base {
     public static void main(String[] args) {
         if(args.length != 3) {
             System.err.println("Usage: lab3.Base <input path> <output path>");
             System.exit(-1);
         }
-        
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }

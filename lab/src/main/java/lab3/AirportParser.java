54c69cadadaa4b4c32594e3ce24dcb6c997d943a
alevko55555 <alevko55555@gmail.com>
2019-11-01 20:16:56 +0300

    Scripted auto-commit on change (2019-11-01 20:16:56) by gitwatch.sh

--- lab/src/main/java/lab3/AirportParser.java
+++ lab/src/main/java/lab3/AirportParser.java
@@ -1,13 +1,16 @@
 package lab3;
 
 public class AirportParser extends BaseParser{
+    private static final int ID_ROW = 0;
+    private static final int NAME_ROW = 1;
+
     public AirportParser(String str) {
         super(str);
     }
     public String getIdAirport() {
-        return getString(0);
+        return getString(ID_ROW);
     }
     public String getInfoAirport() {
-        return getString(1);
+        return getString(NAME_ROW);
     }
 }
--- lab/src/main/java/lab3/FlightParser.java
+++ lab/src/main/java/lab3/FlightParser.java
@@ -3,7 +3,7 @@ package lab3;
 public class FlightParser extends BaseParser{
     private static final int DELAY_COL = 14;
     private static final int DELAY_NEW_COL = 18;
-    private static final  double ZERO = 0.0;
+    private static final  String ZERO_DOUBLE = "0.0";
 
     public FlightParser(String str) {
         super(str);
@@ -13,6 +13,6 @@ public class FlightParser extends BaseParser{
     }
     public String getDelay() {
         String delay = getString(DELAY_NEW_COL);
-        return delay.isEmpty() ? "ZERO" : delay;
+        return delay.isEmpty() ? ZERO_DOUBLE : delay;
     }
 }

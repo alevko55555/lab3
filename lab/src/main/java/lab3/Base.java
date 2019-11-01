f6710497cc2840f75b44ab42b4b8799ab6e99df5
alevko55555 <alevko55555@gmail.com>
2019-11-01 21:45:43 +0300

    Scripted auto-commit on change (2019-11-01 21:45:43) by gitwatch.sh

--- lab/src/main/java/lab3/Base.java
+++ lab/src/main/java/lab3/Base.java
@@ -3,23 +3,31 @@ package lab3;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
+import scala.Tuple2;
 
 import java.util.Map;
 
 public class Base {
     public static void main(String[] args) {
-        if(args.length != 3) {
+        if (args.length != 3) {
             System.err.println("Usage: lab3.Base <input path> <output path>");
             System.exit(-1);
         }
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        final Broadcast<Map<Long, String>> broadcast = sc.broadcast(
-              sc.textFile(args[0])
-                      .zipWithIndex()
-                      .filter(elem->elem._2() != 0).map(elem-> new BaseParser(elem._1()))
-                      .mapToPair()
+        final Broadcast<Map<String, String>> broadcast = sc.broadcast(
+                sc.textFile(args[0])
+                        .zipWithIndex()
+                        .filter(elem -> elem._2() != 0).map(elem -> new AirportParser(elem._1()))
+                        .mapToPair(split ->
+                                new Tuple2<>(
+                                        split.getIdAirport(),
+                                        split.getInfoAirport()
+                                )
+                        )
+                        .collectAsMap()
         );
     }
+}
 

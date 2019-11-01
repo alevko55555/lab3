e1501ac2148d60bf13f3d936c438af954566a924
alevko55555 <alevko55555@gmail.com>
2019-11-01 20:47:03 +0300

    Scripted auto-commit on change (2019-11-01 20:47:03) by gitwatch.sh

--- lab/src/main/java/lab3/FlightSerializable.java
+++ lab/src/main/java/lab3/FlightSerializable.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class FlightSerializable implements Serializable {
     private long maximumLatency;
     private long numberOfFlights;
-    private long 
+    private long numberOfDelayedFlights;
     private long canceledFlight;
     public FlightSerializable(){
 

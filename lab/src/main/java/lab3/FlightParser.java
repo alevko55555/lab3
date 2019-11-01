4d457bbdc93d1bd4adb6fcea11a00769b398beb9
alevko55555 <alevko55555@gmail.com>
2019-11-02 02:15:57 +0300

    Scripted auto-commit on change (2019-11-02 02:15:57) by gitwatch.sh

--- lab/src/main/java/lab3/FlightParser.java
+++ lab/src/main/java/lab3/FlightParser.java
@@ -10,19 +10,22 @@ public class FlightParser extends BaseParser{
     public FlightParser(String str) {
         super(str);
     }
-    public String getIdAirportDest() {
-        String id = getString(ID_AIRPORT_DEST);
-        return id.isEmpty() ? ZERO_DOUBLE : id;
+    public Long getIdAirportDest() {
+        //String id = getString(ID_AIRPORT_DEST);
+        //return id.isEmpty() ? ZERO_DOUBLE : id;
         //return getString(ID_AIRPORT_DEST);
+        return getLong(ID_AIRPORT_DEST);
     }
-    public String getDelay() {
-        String delay = getString(TIME_DELAY);
-        return delay.isEmpty() ? ZERO_DOUBLE : delay;
+    public Long getDelay() {
+        //String delay = getString(TIME_DELAY);
+        //return delay.isEmpty() ? ZERO_DOUBLE : delay;
+        return getLong(TIME_DELAY);
     }
-    public String getIdAirportFrom(){
-        String id = getString(ID_AIRPORT_FROM);
-        return id.isEmpty() ? ZERO_DOUBLE : id;
+    public Long getIdAirportFrom(){
+        //String id = getString(ID_AIRPORT_FROM);
+        //return id.isEmpty() ? ZERO_DOUBLE : id;
         //return getString(ID_AIRPORT_FROM);
+        return getLong(ID_AIRPORT_FROM);
     }
     public Boolean getFlagCanceled() {
         return (long)Double.parseDouble(getString(FLAG_CANCELED)) == 1;
